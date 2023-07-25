3d813586b9414b2901e279ca1e00792fb18f0bd9
anurag200502 <72224463+anurag200502@users.noreply.github.com>
2023-07-25 22:57:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -45,6 +45,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // -- ROUTES WITH FILES
+app.get('/',(req,res)=>{
+  console.log("Hello")
+})
 app.post('/auth/register', upload.single('picture'), register);
 app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
